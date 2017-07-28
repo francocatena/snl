30b27dc7c5048d1dd2a46c16e160dfc90cb0d36a
Franco Catena <francocatena@gmail.com>
2017-07-28 17:00:15 -0300

    Use some alias on session plug

--- lib/snl_web/plugs/session_plug.ex
+++ lib/snl_web/plugs/session_plug.ex
@@ -3,9 +3,12 @@ defmodule SnlWeb.SessionPlug do
   import Plug.Conn
   import SnlWeb.Gettext
 
+  alias Snl.Accounts
+  alias SnlWeb.Router.Helpers
+
   def fetch_current_user(conn, _opts) do
     user_id = get_session(conn, :user_id)
-    user    = user_id && Snl.Accounts.get_user!(user_id)
+    user    = user_id && Accounts.get_user!(user_id)
 
     assign(conn, :current_user, user)
   end
@@ -16,7 +19,7 @@ defmodule SnlWeb.SessionPlug do
     else
       conn
       |> put_flash(:error, gettext("You must be logged in."))
-      |> redirect(to: SnlWeb.Router.Helpers.session_path(conn, :new))
+      |> redirect(to: Helpers.session_path(conn, :new))
       |> halt()
     end
   end
