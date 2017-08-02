c19e2fcdde96e07d85d72371a41ded1aa7a218c8
Franco Catena <francocatena@gmail.com>
2017-08-02 19:45:30 -0300

    Put cache control headers always

--- test/snl_web/plugs/cache_control_plug_test.exs
+++ test/snl_web/plugs/cache_control_plug_test.exs
@@ -3,12 +3,7 @@ defmodule SnlWeb.CacheControlPlugTest do
 
   alias SnlWeb.CacheControlPlug
 
-  setup %{conn: conn} = config do
-    do_setup conn, config[:login_as]
-  end
-
   describe "cache control headers" do
-    @tag login_as: "test@user.com"
     test "put cache control headers correctly when current user", %{conn: conn} do
       conn = CacheControlPlug.put_cache_control_headers(conn, [])
 
@@ -17,13 +12,6 @@ defmodule SnlWeb.CacheControlPlugTest do
       assert get_resp_header(conn, "expires")       == ["Fri, 01 Jan 1990 00:00:00 GMT"]
     end
 
-    test "do not put cache control headers when no current user", %{conn: conn} do
-      conn = CacheControlPlug.put_cache_control_headers(conn, [])
-
-      refute get_resp_header(conn, "cache-control") == ["no-cache, no-store, max-age=0, must-revalidate"]
-    end
-
-    @tag login_as: "test@user.com"
     test "put cache control headers through browser pipe when current user", %{conn: conn} do
       conn =
         conn
@@ -34,12 +22,5 @@ defmodule SnlWeb.CacheControlPlugTest do
       assert get_resp_header(conn, "pragma")        == ["no-cache"]
       assert get_resp_header(conn, "expires")       == ["Fri, 01 Jan 1990 00:00:00 GMT"]
     end
-  end
-
-  defp do_setup(_, nil), do: :ok
-  defp do_setup(conn, _) do
-    conn = assign(conn, :current_user, %Snl.Accounts.User{})
-
-    {:ok, conn: conn}
   end
 end
