b5ca8cd905c27d504d3a8b18f75ca128dd5f3ac8
Franco Catena <francocatena@gmail.com>
2017-08-07 20:05:26 -0300

    Add default admin use seed

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,10 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+{:ok, _} = Snl.Accounts.create_user(%{
+  name:     "Admin",
+  lastname: "Admin",
+  email:    "admin@snl.com",
+  password: "123456"
+})
