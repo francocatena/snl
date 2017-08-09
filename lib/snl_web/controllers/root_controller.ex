8f3b8606aba345739651ae8f90fcb7998fada088
Franco Catena <francocatena@gmail.com>
2017-08-09 11:28:37 -0300

    Root redirects to new session or user index
    If user is logged in goes to user index, new session otherwise

--- lib/snl_web/controllers/root_controller.ex
+++ lib/snl_web/controllers/root_controller.ex
@@ -1,6 +1,11 @@
 defmodule SnlWeb.RootController do
   use SnlWeb, :controller
 
+  def index(%{assigns: %{current_account: account}} = conn, _params)
+  when is_map(account) do
+    redirect(conn, to: user_path(conn, :index))
+  end
+
   def index(conn, _params) do
     redirect(conn, to: session_path(conn, :new))
   end
--- test/snl_web/controllers/root_controller_test.exs
+++ test/snl_web/controllers/root_controller_test.exs
@@ -1,11 +1,19 @@
 defmodule SnlWeb.RootControllerTest do
   use SnlWeb.ConnCase
+  use Snl.Support.LoginHelper
 
   describe "index" do
     test "redirect to new session", %{conn: conn} do
       conn = get(conn, root_path(conn, :index))
 
       assert redirected_to(conn) == session_path(conn, :new)
+    end
+
+    @tag login_as: "test@user.com"
+    test "redirect to user's index when logged in", %{conn: conn} do
+      conn = get(conn, root_path(conn, :index))
+
+      assert redirected_to(conn) == user_path(conn, :index)
     end
   end
 end
